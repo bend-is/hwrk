29d00c2ac945a9e294a26040eec548847a9d931d
FrostBlade001 <udov2008@gmail.com>
2022-06-01 00:24:51 +0500

    implement linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,82 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len        int
+	head, tail *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int         { return l.len }
+func (l *list) Back() *ListItem  { return l.tail }
+func (l *list) Front() *ListItem { return l.head }
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	l.len++
+
+	item := &ListItem{Value: v}
+
+	if l.head == nil {
+		l.head = item
+		l.tail = item
+
+		return item
+	}
+
+	item.Next = l.head
+	l.head.Prev = item
+	l.head = item
+
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	l.len++
+
+	item := &ListItem{Value: v}
+
+	if l.head == nil {
+		l.head = item
+		l.tail = item
+
+		return item
+	}
+
+	item.Prev = l.tail
+	l.tail.Next = item
+	l.tail = item
+
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	l.len--
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if l.head == i {
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	i.Next = l.head
+	l.head.Prev = i
+	l.head = i
 }
